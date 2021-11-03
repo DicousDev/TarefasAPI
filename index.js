dfa515d5717a7851c298b27b4b2b05ab887cfb77
João Victor Duarte <joaodicu387@gmail.com>
2021-11-03 03:24:17 -0300

    Editando tarefa especifica

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-const taskList = [{description: "ESTUDAR", priority: "MÁXIMA", stateTask: "ANDAMENTO"}]
+
+let taskCount = 1;
+const taskList = [{
+    id: 1,
+    description: "ESTUDAR", 
+    priority: "MÁXIMA", 
+    stateTask: "ANDAMENTO", 
+}]
 
 app.get("/", (req, res) => {
     res.json({"message": "Lista de projetos API"});
@@ -14,10 +21,38 @@ app.get("/tasks", (req, res) => {
 });
 
 app.post("/generateTask", (req, res) => {
+    taskCount += 1;
     const {description, priority, stateTask} = req.body;
-    taskList.push({description: description, priority: priority, stateTask: stateTask});
-    res.json({"code":200 ,"message": "Tarefa adicionada com sucesso!"})
+    taskList.push({id: taskCount, description: description, priority: priority, stateTask: stateTask});
+    res.json({
+        "code":201,
+        "body":{id: taskCount, description: description, priority: priority, stateTask: stateTask},
+        "message": "Tarefa adicionada com sucesso."})
 });
+
+app.patch("/setTask/:id", (req, res) => {
+    const {description, priority, stateTask} = req.body;
+
+    if (req.params.id - 1 > taskList.length - 1) {
+        return res.json({"code": 404, "message": "Essa tarefa não existe."});
+    }
+
+    if (description) {
+        taskList[req.params.id - 1].description = description; 
+    }
+    
+    if(priority)
+        taskList[req.params.id - 1].priority = priority;
+    
+    if (stateTask) {
+        taskList[req.params.id - 1].stateTask = stateTask;
+    }
+
+    res.json({
+        "code":200,
+        "body": taskList[req.params.id - 1], 
+        "message": `Tarefa ${req.params.id} modificada com sucesso.`});
+})
 
 app.listen(port, () => {
     console.log(`Server running in port ${port} 🚀`)
